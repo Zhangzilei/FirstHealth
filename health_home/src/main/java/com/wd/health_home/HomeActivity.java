0a679339e1acc01e0ce9504e2b968b3e57582c8c
zzzl <1946298407@qq.com>
2019-12-04 09:45:37 +0800

    框架創建完成

---
+++ health_home/src/main/java/com/wd/health_home/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.wd.health_home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wd.common.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView();
+//
+//    }
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_home;
+    }
+}
